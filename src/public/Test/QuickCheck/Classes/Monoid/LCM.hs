c06e065d5089cc6c3557553c7ba4870066ffa033
Jonathan Knowles <mail@jonathanknowles.net>
2023-03-27 06:41:10 +0000

    Add laws for distributive LCM monoids.

--- src/public/Test/QuickCheck/Classes/Monoid/LCM.hs
+++ src/public/Test/QuickCheck/Classes/Monoid/LCM.hs
@@ -11,6 +11,7 @@
 --
 module Test.QuickCheck.Classes.Monoid.LCM
     ( lcmMonoidLaws
+    , distributiveLCMMonoidLaws
     )
     where
 
@@ -24,7 +25,7 @@ import Data.Maybe
 import Data.Monoid.GCD
     ( GCDMonoid (..) )
 import Data.Monoid.LCM
-    ( LCMMonoid (..) )
+    ( DistributiveLCMMonoid, LCMMonoid (..) )
 import Data.Proxy
     ( Proxy (..) )
 import Data.Semigroup.Cancellative
@@ -410,3 +411,106 @@ lcmMonoidLaw_absorption_lcm_gcd a b =
     & report
         "gcd a (lcm a b)"
         (gcd a (lcm a b))
+
+--------------------------------------------------------------------------------
+-- DistributiveLCMMonoid
+--------------------------------------------------------------------------------
+
+-- | 'Laws' for instances of 'DistributiveLCMMonoid'.
+--
+-- Includes the following laws:
+--
+-- @
+-- 'lcm' (a '<>' b) (a '<>' c) '==' a '<>' 'lcm' b c
+-- @
+-- @
+-- 'lcm' (a '<>' c) (b '<>' c) '==' 'lcm' a b '<>' c
+-- @
+-- @
+-- 'lcm' a ('gcd' b c) '==' 'gcd' ('lcm' a b) ('lcm' a c)
+-- @
+-- @
+-- 'gcd' a ('lcm' b c) '==' 'lcm' ('gcd' a b) ('gcd' a c)
+-- @
+--
+-- Note that the following superclass laws are __not__ included:
+--
+-- * 'Test.QuickCheck.Classes.Monoid.GCD.distributiveGCDMonoidLaws'
+-- * 'lcmMonoidLaws'
+--
+distributiveLCMMonoidLaws
+    :: forall a. (Arbitrary a, Show a, Eq a, DistributiveLCMMonoid a)
+    => Proxy a
+    -> Laws
+distributiveLCMMonoidLaws _ = Laws "DistributiveLCMMonoid"
+    [ makeLaw3 @a
+        "distributiveLCMMonoidLaw_distributivity_left"
+        (distributiveLCMMonoidLaw_distributivity_left)
+    , makeLaw3 @a
+        "distributiveLCMMonoidLaw_distributivity_right"
+        (distributiveLCMMonoidLaw_distributivity_right)
+    , makeLaw3 @a
+        "distributiveLCMMonoidLaw_distributivity_gcd_lcm"
+        (distributiveLCMMonoidLaw_distributivity_gcd_lcm)
+    , makeLaw3 @a
+        "distributiveLCMMonoidLaw_distributivity_lcm_gcd"
+        (distributiveLCMMonoidLaw_distributivity_lcm_gcd)
+    ]
+
+distributiveLCMMonoidLaw_distributivity_left
+    :: (Eq a, Show a, DistributiveLCMMonoid a) => a -> a -> a -> Property
+distributiveLCMMonoidLaw_distributivity_left a b c =
+    makeProperty
+        "lcm (a <> b) (a <> c) == a <> lcm b c"
+        (lcm (a <> b) (a <> c) == a <> lcm b c)
+    & report
+        "a <> b"
+        (a <> b)
+    & report
+        "a <> c"
+        (a <> c)
+    & report
+        "lcm (a <> b) (a <> c)"
+        (lcm (a <> b) (a <> c))
+    & report
+        "lcm b c"
+        (lcm b c)
+    & report
+        "a <> lcm b c"
+        (a <> lcm b c)
+
+distributiveLCMMonoidLaw_distributivity_right
+    :: (Eq a, Show a, DistributiveLCMMonoid a) => a -> a -> a -> Property
+distributiveLCMMonoidLaw_distributivity_right a b c =
+    makeProperty
+        "lcm (a <> c) (b <> c) == lcm a b <> c"
+        (lcm (a <> c) (b <> c) == lcm a b <> c)
+    & report
+        "a <> c"
+        (a <> c)
+    & report
+        "b <> c"
+        (b <> c)
+    & report
+        "lcm (a <> c) (b <> c)"
+        (lcm (a <> c) (b <> c))
+    & report
+        "lcm a b"
+        (lcm a b)
+    & report
+        "lcm a b <> c"
+        (lcm a b <> c)
+
+distributiveLCMMonoidLaw_distributivity_gcd_lcm
+    :: (Eq a, Show a, DistributiveLCMMonoid a) => a -> a -> a -> Property
+distributiveLCMMonoidLaw_distributivity_gcd_lcm a b c =
+    makeProperty
+        "lcm a (gcd b c) == gcd (lcm a b) (lcm a c)"
+        (lcm a (gcd b c) == gcd (lcm a b) (lcm a c))
+
+distributiveLCMMonoidLaw_distributivity_lcm_gcd
+    :: (Eq a, Show a, DistributiveLCMMonoid a) => a -> a -> a -> Property
+distributiveLCMMonoidLaw_distributivity_lcm_gcd a b c =
+    makeProperty
+        "gcd a (lcm b c) == lcm (gcd a b) (gcd a c)"
+        (gcd a (lcm b c) == lcm (gcd a b) (gcd a c))
